1cabf379346bc47a8e792afac473e24d8e4a33de
danny <guanome@gmail.com>
2018-01-09 22:27:46 -0600

    initial commit

---
+++ IsItSnowing/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace IsItSnowing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCurrentTemp = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSnowing = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current Temp";
+            // 
+            // lblCurrentTemp
+            // 
+            this.lblCurrentTemp.AutoSize = true;
+            this.lblCurrentTemp.BackColor = System.Drawing.SystemColors.Control;
+            this.lblCurrentTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentTemp.Location = new System.Drawing.Point(12, 49);
+            this.lblCurrentTemp.Name = "lblCurrentTemp";
+            this.lblCurrentTemp.Size = new System.Drawing.Size(126, 55);
+            this.lblCurrentTemp.TabIndex = 2;
+            this.lblCurrentTemp.Text = "35°F";
+            this.lblCurrentTemp.DoubleClick += new System.EventHandler(this.lblCurrentTemp_DoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 17);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 129);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(340, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged_1);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Is it snowing?";
+            // 
+            // lblSnowing
+            // 
+            this.lblSnowing.AutoSize = true;
+            this.lblSnowing.BackColor = System.Drawing.SystemColors.Control;
+            this.lblSnowing.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSnowing.Location = new System.Drawing.Point(154, 49);
+            this.lblSnowing.Name = "lblSnowing";
+            this.lblSnowing.Size = new System.Drawing.Size(86, 55);
+            this.lblSnowing.TabIndex = 5;
+            this.lblSnowing.Text = "No";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 151);
+            this.Controls.Add(this.lblSnowing);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.lblCurrentTemp);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Is It Snowing?";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCurrentTemp;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSnowing;
+    }
+}
+
---
+++ IsItSnowing/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using IsItSnowing.Models;
+
+namespace IsItSnowing
+{
+    public partial class Form1 : Form
+    {
+        private static string ApiUrl;
+        private static Config AppConfig;
+        public static string CacheFolder;
+        public const int UpdateInterval = 150000;
+
+        public CurrentConditionsWeatherResponse CurrentWeather;
+        public Forecast Forecast;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            AppConfig = Newtonsoft.Json.JsonConvert.DeserializeObject<Config>(System.IO.File.ReadAllText(System.IO.Directory.GetCurrentDirectory() + "\\Config.json"));
+            if (AppConfig.ApiKey == null)
+                throw new ArgumentNullException("Missing API KEY in Config.json file");
+            ApiUrl = string.Format("http://api.wunderground.com/api/{0}/", AppConfig.ApiKey);
+
+            CacheFolder = System.IO.Directory.GetCurrentDirectory() + "\\CachedWeather";
+            System.IO.Directory.CreateDirectory(CacheFolder);
+            timer1.Interval = UpdateInterval;
+            backgroundWorker1.RunWorkerAsync();
+            timer1.Start();
+        }
+
+        public void GetWeather()
+        {           
+
+            CurrentWeather = GetCurrentWeather(AppConfig.City, AppConfig.State);
+            Forecast = GetForecast(AppConfig.City, AppConfig.State);
+        }
+
+        public void CacheCurrentWeather(Models.CurrentConditionsWeatherResponse weather)
+        {
+            var file = CacheFolder + "\\CurrentWeather.json";
+            System.IO.File.WriteAllText(file, Newtonsoft.Json.JsonConvert.SerializeObject(weather));
+        }
+
+        public JsonSerializerSettings JsonSerializerSettings()
+        {
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            settings.Formatting = Formatting.Indented;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            return settings;
+        }
+
+        public void CacheForecast(Models.Forecast forecast)
+        {
+            var file = CacheFolder + "\\Forecast.json";
+            System.IO.File.WriteAllText(file, Newtonsoft.Json.JsonConvert.SerializeObject(forecast));
+        }
+
+        public Models.CurrentConditionsWeatherResponse GetCurrentWeather(string city, string state)
+        {
+            var cachedFile = new FileInfo(CacheFolder + "\\CurrentWeather.json");
+            Models.CurrentConditionsWeatherResponse weather = null;
+            if (cachedFile.Exists && (DateTime.Now - cachedFile.LastWriteTime).TotalSeconds < UpdateInterval)
+            {
+                weather = Newtonsoft.Json.JsonConvert.DeserializeObject<Models.CurrentConditionsWeatherResponse>(File.ReadAllText(cachedFile.FullName), JsonSerializerSettings());
+            }
+            else
+            {
+                var client = new System.Net.WebClient {Proxy = {Credentials = CredentialCache.DefaultCredentials}};
+                var weatherJson = client.DownloadString($"{ApiUrl}conditions/q/{state}/{city}.json");
+                weather = Newtonsoft.Json.JsonConvert.DeserializeObject<Models.CurrentConditionsWeatherResponse>(weatherJson, JsonSerializerSettings());
+                if (weather.current_observation != null)
+                    CacheCurrentWeather(weather);
+                else
+                    MessageBox.Show($"Weather returned null, please check your API key, city, or state in the Config.json file. Current API Key: {AppConfig.ApiKey}");
+            }
+
+            return weather;
+        }
+
+        public Models.Forecast GetForecast(string city, string state)
+        {
+            var cachedFile = new FileInfo(CacheFolder + "\\Forecast.json");
+            Models.Forecast forecast = null;
+            if (cachedFile.Exists && (DateTime.Now - cachedFile.LastWriteTime).TotalSeconds < UpdateInterval)
+            {
+                forecast = Newtonsoft.Json.JsonConvert.DeserializeObject<Models.Forecast>(File.ReadAllText(cachedFile.FullName), JsonSerializerSettings());
+            }
+            else
+            {
+                var client = new System.Net.WebClient {Proxy = {Credentials = CredentialCache.DefaultCredentials}};
+                var weatherJson = client.DownloadString($"{ApiUrl}forecast/q/{state}/{city}.json");
+                forecast = Newtonsoft.Json.JsonConvert.DeserializeObject<Models.Forecast>(weatherJson, JsonSerializerSettings());
+                if (forecast.simpleforecast != null)
+                    CacheForecast(forecast);
+                else
+                    MessageBox.Show($"Forecast returned null, please check your API key, city, or state in the Config.json file. Current API Key: {AppConfig.ApiKey}");                
+            }
+            return forecast;
+        }
+
+        public void RefreshWeather(BackgroundWorker worker)
+        {
+            worker.ReportProgress(0, "Getting weather");
+            GetWeather();
+            worker.ReportProgress(100, "Displaying weather");
+        }
+
+        public void UpdateUI()
+        {
+            if (CurrentWeather.current_observation != null)
+            {
+                var tempInF = Math.Round(CurrentWeather.current_observation.temp_f);
+                lblCurrentTemp.Text = tempInF.ToString() + "°F";
+                if (tempInF <= 32)
+                    lblCurrentTemp.ForeColor = Color.Aqua;
+                else
+                    lblCurrentTemp.ForeColor = Color.DarkSlateGray;
+                var isSnowing = (CurrentWeather.current_observation.icon.ToLower().Contains("snow") || CurrentWeather.current_observation.icon.ToLower().Contains("flur"));
+                lblSnowing.Text = isSnowing ? "Yes" : "No";
+
+                if (isSnowing)
+                {
+                    lblSnowing.ForeColor = Color.WhiteSmoke;
+                    lblSnowing.BackColor = Color.Black;
+                }
+                else
+                {
+                    lblSnowing.ForeColor = Color.Black;
+                    lblSnowing.BackColor = Color.Transparent;
+                }
+
+                lblStatus.Text = $"Refreshed {DateTime.Now.ToString("h:mm:ss tt").ToUpper()}";
+            }
+            else
+            {
+                lblStatus.Text = $"Error getting weather";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var worker = (BackgroundWorker)sender;
+            RefreshWeather(worker);
+        }
+
+        private void backgroundWorker1_ProgressChanged_1(object sender, ProgressChangedEventArgs e)
+        {
+            var output = $"{DateTime.Now.ToString("hh:mm:ss")} {e.UserState}";
+            lblStatus.Text = output;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            UpdateUI();
+        }
+
+        private void lblCurrentTemp_DoubleClick(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+    }
+}
